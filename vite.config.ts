d579e1fb0211bcb86f18ee7ca69058ac1eb5b72c
xxxc <15790480+xxxcdsfd@user.noreply.gitee.com>
2025-08-31 15:49:29 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path'; // 确保正确导入了 path
 
-// https://vite.dev/config/
+//elementPlus按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver({ importStyle: 'sass' })],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // 使用 path.resolve
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
+        `,
+      }
+    }
+  }
 });
